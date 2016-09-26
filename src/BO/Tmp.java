7431a75abe08e4509a6e3dd27d347f60bc0062d3
marthel <marthel@kth.se>
2016-09-26 15:53:38 +0200

    skit

---
+++ src/BO/Tmp.java
@@ -0,0 +1,7 @@
+package BO;
+
+/**
+ * Created by Marthin on 2016-09-26.
+ */
+public class Tmp {
+}
---
+++ src/DB/Tmp.java
@@ -0,0 +1,7 @@
+package DB;
+
+/**
+ * Created by Marthin on 2016-09-26.
+ */
+public class Tmp {
+}
