947d258cd8022f05d75e96cabe69f1802c2f74cf
marthel <marthel@kth.se>
2016-10-02 18:35:16 +0200

    asdasd

--- src/UI/LoginServlet.java
+++ src/UI/LoginServlet.java
@@ -6,7 +6,6 @@ import UI.Models.UiUser;
 
 import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
