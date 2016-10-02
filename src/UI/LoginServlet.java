f4d5978fdda150a00270ab9746c3cdbe788867ab
marthel <marthel@kth.se>
2016-10-02 09:47:35 +0200

    awd

--- src/UI/LoginServlet.java
+++ src/UI/LoginServlet.java
@@ -13,11 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-/**
- * Created by Marthin on 2016-10-02.
- */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
